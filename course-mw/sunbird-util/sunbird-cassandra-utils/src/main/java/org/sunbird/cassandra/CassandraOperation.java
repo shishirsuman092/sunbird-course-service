97a35c9e969c62c3d1b3a7467dc274f870bc785e
Shishir Suman <shishir.suman@tarento.com>
2023-03-30 14:56:22 +0530

    adding code changes for admin flow

--- course-mw/sunbird-util/sunbird-cassandra-utils/src/main/java/org/sunbird/cassandra/CassandraOperation.java
+++ course-mw/sunbird-util/sunbird-cassandra-utils/src/main/java/org/sunbird/cassandra/CassandraOperation.java
@@ -58,7 +58,10 @@ public interface CassandraOperation {
    */
   public Response deleteRecord(String keyspaceName, String tableName, String identifier, RequestContext requestContext);
 
-  /**
+    Response getRecordByIndexedPropertyPagination(
+            String keyspaceName, String tableName, Map<String, Object> params, RequestContext requestContext);
+
+    /**
    * @desc This method is used to delete record in cassandra db by their primary composite key
    * @param keyspaceName Keyspace name
    * @param tableName Table name
